e26325fa63bb48d9e79bcdcf2f2ab93c8cafc86f
Kokoro0610 <kokorohidari@gmail.com>
2025-02-10 11:51:57 +0900

    Role選択と初期情報登録前後の画面遷移とデータフロー整備

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_app1/screens/auth/Child_Parent_select_screen.dart';
-// import 'screens/child/c_start_intro_screen.dart';
+import 'package:flutter_app1/screens/auth/role_selection_screen.dart';
+import 'package:flutter_app1/screens/child/c_home_screen.dart';
+import 'package:flutter_app1/screens/parent/p_home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  // 登録済みなら、"c_registered" または "p_registered" が格納されている
+  final isRegistered = prefs.getString('isRegistered');
+
+  Widget homeWidget;
+  if (isRegistered == null) {
+    // 未登録の場合はロール選択画面へ
+    homeWidget = const RoleSelectionScreen();
+  } else if (isRegistered == 'c_registered') {
+    homeWidget = const CHomeScreen(
+      //子供のおかえり画面
+      //TODO:一旦仮置き
+      userName: 'あい',
+      loginDays: 10,
+    );
+  } else if (isRegistered == 'p_registered') {
+    //親のおかえり画面
+    homeWidget = const PHomeScreen();
+  } else {
+    // 万が一（不正な値が入っていた場合）はロール選択画面を表示
+    homeWidget = const RoleSelectionScreen();
+  }
+
+  runApp(MyApp(homeWidget: homeWidget));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget homeWidget;
+  const MyApp({Key? key, required this.homeWidget}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Child App',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const CPselectScreen(),
+      title: 'ユーザ選択アプリ',
+      debugShowCheckedModeBanner: false,
+      home: homeWidget,
     );
   }
 }
